e82c3f939945ab06c0c5a3313bac6aee6eb89c98
wuxiangyu <wuxaingyu.lc@bytedance.com>
2019-09-18 18:28:10 +0800

    feat: 倒入第三方
    Change-Id: I56bfc74420434fc8fc492a00f2271e91db9addfb

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var wordPair = new WordPair.random();
     return new MaterialApp(
         title: "welcome to flutter",
         home: new Scaffold(
@@ -12,7 +16,7 @@ class MyApp extends StatelessWidget {
             title: new Text("welcome to flutter"),
           ),
           body: new Center(
-            child: new Text("hello world"),
+            child: new Text(wordPair.asPascalCase),
           ),
         ));
   }
